eff64c653bfd38f1fc852a569fc186aed29d6fa0
113075HD57074\Administrator <arthur.kamsu@lemak-soft.com>
2018-11-14 13:17:04 -0600

    The API project created and Swagger added + a test project created

--- src/WebApi/CompanyXApi/CompanyXApi/Startup.cs
+++ src/WebApi/CompanyXApi/CompanyXApi/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
+
 namespace CompanyXApi
 {
     public class Startup
@@ -25,6 +26,32 @@ namespace CompanyXApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddSwaggerGen
+                (
+                    Options => 
+                    {
+                        Options.SwaggerDoc("v0.1.0",new Swashbuckle.AspNetCore.Swagger.Info
+                            {
+                                Title = "CompanyX API Developer Guide",
+                                Description = "This API is for CRUD operations on a fictif company \"CompanyX\" employees database.",
+                                Version = "0.1.0",
+                                Contact =new Swashbuckle.AspNetCore.Swagger.Contact
+                                {
+                                    Email="arthur.kamsu.m@gmail.com",
+                                    Name="Arthur Kamsu",
+                                    Url="http://arthurkamsu.me"
+                                },                               
+                                License=new Swashbuckle.AspNetCore.Swagger.License
+                                {
+                                    Name="CompanyX API License v0.1.0",
+                                    Url="http://compamyxlicenses.com/api-v-0.1.0"
+                                },
+                                TermsOfService = "http://compamyxtos.com/api-v-0.1.0"                                
+                            }
+                            
+                        );
+                    }
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,6 +62,13 @@ namespace CompanyXApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSwagger();
+            app.UseSwaggerUI(
+                c => {
+                    c.SwaggerEndpoint("/swagger/v0.1.0/swagger.json", "CompanyX API v0.1.0");
+                    c.RoutePrefix = string.Empty;                    
+                }
+            );
             app.UseMvc();
         }
     }
