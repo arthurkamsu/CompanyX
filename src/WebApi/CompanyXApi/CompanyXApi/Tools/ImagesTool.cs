e51d997ccb7d33b102305540358b6fa473fe6836
113075HD57074\Administrator <arthur.kamsu@lemak-soft.com>
2018-11-16 16:26:41 -0600

    Introducing a tool for handling images conversion from base 64 to Image and vice versa

---
+++ src/WebApi/CompanyXApi/CompanyXApi/Tools/ImagesTool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CompanyXApi.Tools
+{
+    public class ImagesTool
+    {
+       /* public static System.Drawing. Base64ToImage(string base64String)
+        {
+            // Convert base 64 string to byte[]
+            byte[] imageBytes = Convert.FromBase64String(base64String);
+            // Convert byte[] to Image
+            using (var ms = new MemoryStream(imageBytes, 0, imageBytes.Length))
+            {
+                System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+                return image;
+            }
+        }*/
+    }
+}
